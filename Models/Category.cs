648cb043758fad662f13587068ca01171e6963af
Anna Pittman <annaepittman@gmail.com>
2022-01-04 09:13:04 -0800

    edit

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,11 +6,11 @@ namespace ToDoList.Models
     {
         public Category()
         {
-            this.Items = new HashSet<Item>();
+            this.JoinEntities = new HashSet<CategoryItem>(); //Collection navigation property - contains a reference to many related Items. Without this property, we won't be able to access related Items in our controllers and views
         }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<CategoryItem> JoinEntities { get; set; }
     }
 }
--- Models/ToDoListContext.cs
+++ Models/ToDoListContext.cs
@@ -6,7 +6,8 @@ namespace ToDoList.Models
   {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
-
+    public DbSet<CategoryItem> CategoryItem { get; set; } //each DbSet will become a table in our database
+  
     public ToDoListContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
